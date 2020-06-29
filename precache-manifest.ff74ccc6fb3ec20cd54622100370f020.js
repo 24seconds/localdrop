7a0e9a408d700556b09cae42ae1af40be01718d8
GeunYeong Cheon <24crazyoung@gmail.com>
2020-06-30 03:49:14 +0900

    Updates

---
+++ precache-manifest.ff74ccc6fb3ec20cd54622100370f020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1814e7ddc56694b655d252a4ec4021d4",
+    "url": "/localdrop/index.html"
+  },
+  {
+    "revision": "96868f2c8482d7811895",
+    "url": "/localdrop/static/css/main.aed9d2e1.chunk.css"
+  },
+  {
+    "revision": "a15ef19be67fc22d5cf2",
+    "url": "/localdrop/static/js/2.f39b9c61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/localdrop/static/js/2.f39b9c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96868f2c8482d7811895",
+    "url": "/localdrop/static/js/main.b30377ee.chunk.js"
+  },
+  {
+    "revision": "9a681d1d4853196c3aa0",
+    "url": "/localdrop/static/js/runtime-main.53291969.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/localdrop/static/media/github-logo.76d344f0.svg"
+  }
+]);
